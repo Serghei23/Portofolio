904ef996c4116f16f51c6540bbe725c4600d1937
Serghei23 <88316613+Serghei23@users.noreply.github.com>
2021-08-18 11:48:17 +0300

    Portofolio

---
+++ js/animation.js
@@ -0,0 +1,12 @@
+$('.slide').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: true,
+  autoplay:true,
+  autoplaySpeed:1500
+});
+
+  //let array=['rosu','galben','albastru']
+
+  
